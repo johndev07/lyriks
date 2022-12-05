15a23473830f6eb58d3ef7da6a81bce662f8a6b9
johndev07 <alwynjohn.dev@gmail.com>
2022-12-05 10:32:03 +0530

    topchart pages import chnaged

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 import { Error, Loader, SongCard } from "../components";
-import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { useGetTopChartsQuery } from "../redux/services/shazamcore.js";
 
 const TopCharts = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
